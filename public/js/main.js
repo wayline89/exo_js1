8e24db9666e6098253a48820969b82de847cf109
wayline89 <laurentdugardin@gmail.com>
2022-12-19 12:35:12 +0100

    exo1 et 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,90 @@
+// EXO 1 
+
+// let A = 3;
+// let B = 2;
+// let C = A + B;
+// let tartine = "j'aime les tartines et je mange "
+
+// console.log(C); 
+
+// let parjour = " 4 par jours"
+
+// tartine + parjour
+
+// console.log(tartine + parjour); 
+
+// let trois = 5
+// let quatre = 35
+
+// trois + quatre
+
+// console.log(trois + quatre); 
+
+
+// tartine + trois
+
+// console.log(tartine + trois); 
+
+// let phrase = "salut tt le monde"
+
+// console.log(phrase)
+
+// let phrase2 = "au revoir !"
+
+// console.log(phrase2)
+
+// phrase=phrase2
+
+// console.log(phrase)
+
+// let operateur = 4
+// let operateur2 = "6"
+
+// operateur>operateur2
+
+// operateur<operateur2
+
+// operateur==operateur2
+
+// operateur!=operateur2
+
+// operateur===operateur2
+
+// operateur!==operateur2
+
+// let operateur3 = 4
+// let operateur4 = 6
+
+// operateur3>operateur4
+
+// operateur3<operateur4
+
+// operateur3==operateur4
+
+// operateur3!=operateur4
+
+// operateur3===operateur4
+
+// operateur3==operateur4
+
+
+// let operateur5 = "4"
+// let operateur6 = "6"
+
+// operateur5>operateur6
+
+// operateur5<operateur6
+
+// operateur5==operateur6
+
+// operateur5!=operateur6
+
+// operateur5===operateur6
+
+// operateur5!==operateur6
+
+// EXO2 :
+
 // let A = 3;
 // let B = 2;
 // let C = A + B;
