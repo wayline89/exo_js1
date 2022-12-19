0017cde7da43c0a0d469f481d3457cc5cccbb2ba
wayline89 <laurentdugardin@gmail.com>
2022-12-19 12:10:50 +0100

    premier

---
+++ public/js/main.js
@@ -0,0 +1,57 @@
+// let A = 3;
+// let B = 2;
+// let C = A + B;
+
+// console.log(C);
+
+// EXO1
+
+// let prenom  = "laurent"
+
+// console.log(prenom);
+
+// let age  = 33
+
+// console.log(age);
+
+// console.log(`${prenom} ${age}`);
+
+// alert(`${prenom} ${age}`);
+
+// Exo2
+
+// let prenomexo2 = window.prompt("prenomexo2");
+// let ageexo2 = window.prompt("ageexo2");
+
+// let text = " ton nom " + prenomexo2 + " ton age " + ageexo2 ;
+
+// console.log(text);
+
+// alert(`${text}`);
+
+// EXO 3
+
+// let chiffre1 = window.prompt("chiffre1");
+// let chiffre2 = window.prompt("chiffre2");
+// chiffre1 = Number(chiffre1);
+// chiffre2 = Number(chiffre2);
+
+// let plus = chiffre1 + chiffre2;
+// let moins = chiffre1 - chiffre2;
+// let multiplication = chiffre1 * chiffre2;
+// let division = chiffre1 / chiffre2;
+
+// console.log(plus)
+// console.log(moins)
+// console.log(multiplication)
+// console.log(division)
+
+// EXO 4
+
+// let phrase = prompt('phrase');
+// let phrase2 = prompt('mot à changer');
+// let replacement = prompt('nouvelleph');
+// let modifiedPhrase = phrase.replace(phrase2, replacement);
+// console.log(modifiedPhrase);
+// alert(`${modifiedPhrase}`);
+
